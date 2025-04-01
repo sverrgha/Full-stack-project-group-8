2747efcbadadb86d95feba761198d061aaf732b4
johanarntsen <johamar@stud.ntnu.no>
2025-04-01 18:09:31 +0200

    refactor: change variable phonenumber to String, and simplified class to lombok-layout

--- backend/src/main/java/ntnu/idatt2105/project/backend/model/User.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/model/User.java
@@ -6,6 +6,11 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 /*
  * This class represents a user in the system.
  * It contains the user's information such as username, password, and roles.
@@ -13,7 +18,9 @@ import org.springframework.data.relational.core.mapping.Table;
  * It is part of the backend model package and is used in conjunction with the UserRepo interface
  * to interact with the database.
  */
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table("users")
 public class User {
     
@@ -30,7 +37,7 @@ public class User {
     private String email;
     
     @Column("phonenumber")
-    private long phonenumber;
+    private String phonenumber;
     
     @Column("password")
     private String password;
@@ -40,68 +47,6 @@ public class User {
 
     @Column("createdAt")
     private Date createdAt;
-    
-
-    public User() {
-    }
-
-    public User(long id, String firstname, String lastname, String email, long phonenumber, String password) {
-        this.id = id;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.phonenumber = phonenumber;
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getPhonenumber() {
-        return phonenumber;
-    }
-
-    public void setPhonenumber(long phonenumber) {
-        this.phonenumber = phonenumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
 }
 
     
