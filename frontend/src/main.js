9c040de2b77c1b73aedd410007a02014d51b7ad0
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2025-04-01 14:26:35 +0200

    feat: add support for multiple languages with i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n.js'
 
 createApp(App)
     .use(router)
+    .use(i18n)
     .mount('#app')
