1e542ccf9ab890702181c65b0b6d6f8f1f2c31d4
emilruud <emilruud@ntnu.no>
2025-04-06 12:26:59 +0200

    fixed routes in navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -45,10 +45,9 @@ onUnmounted(() => {
       <!-- Navigation Links -->
       <ul class="nav-links" :class="{ 'open': isMenuOpen }">
         <nav-bar-item to="/products" text="products"/>
-        <nav-bar-item to="#" text="notifications"/>
-        <nav-bar-item to="#" text="newListing"/>
-        <nav-bar-item to="#" text="messages"/>
-        <nav-bar-item to="/profile" text="profile"/>        
+        <nav-bar-item to="/new-listing" text="newListing"/>
+        <nav-bar-item to="/messages" text="messages"/>
+        <nav-bar-item to="/profile" text="profile"/>
         <li>
           <div class="language-toggle" @click="toggleLanguage">
             <img :src="locale === 'no' ? flagNo : flagEn"
