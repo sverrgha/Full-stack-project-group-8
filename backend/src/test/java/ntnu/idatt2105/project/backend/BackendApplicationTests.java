7a39272b7aad282194a1905be7c12712063ea188
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2025-04-08 20:20:13 +0200

    fix: try to fix bug where pipeline fails the build, while it works locally

--- backend/src/test/java/ntnu/idatt2105/project/backend/BackendApplicationTests.java
+++ backend/src/test/java/ntnu/idatt2105/project/backend/BackendApplicationTests.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class BackendApplicationTests {
 
 	@Container
@@ -17,12 +21,16 @@ class BackendApplicationTests {
 					.withUsername("test")
 					.withPassword("test");
 
-	@BeforeAll
-	static void beforeAll() {
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
 		mysql.start();
 		System.setProperty("spring.datasource.url", mysql.getJdbcUrl());
 		System.setProperty("spring.datasource.username", mysql.getUsername());
 		System.setProperty("spring.datasource.password", mysql.getPassword());
+	}
+
+	@Test
+	void contextLoads() {
 	}
 
 }
