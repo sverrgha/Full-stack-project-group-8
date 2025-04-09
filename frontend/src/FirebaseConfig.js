a361152dc8c2bd1f17c89c3dc36216c3a0c637ee
Anders <alunde@stud.ntnu.no>
2025-04-09 12:04:47 +0200

    minor changes

--- frontend/src/FirebaseConfig.js
+++ frontend/src/FirebaseConfig.js
@@ -1,9 +1,9 @@
+// Copied from Firebase website
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
