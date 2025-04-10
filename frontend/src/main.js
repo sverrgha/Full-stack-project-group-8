ad4da1ea742fd75607d8dd8fc7d29fee23ecf9e1
emilruud <emilruud@ntnu.no>
2025-04-10 12:24:05 +0200

    improved documentation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n.js'
-import { useAuthStore } from './stores/auth' // Import the auth store
+import { useAuthStore } from './stores/auth'
 
 const pinia = createPinia()
 const app = createApp(App)
