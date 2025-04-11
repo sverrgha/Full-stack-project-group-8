0a3b7c421a8545b1663af3d94f9a99a67daf5622
Anders <alunde@stud.ntnu.no>
2025-04-11 06:00:57 +0200

    minor fix

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:5173',
         setupNodeEvents(on, config) {
-            // implement node event listeners here
         },
     },
 });
